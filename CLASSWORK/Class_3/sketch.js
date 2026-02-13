d39272668dec1f5242409bf331e004893299cc92
O5-XII <05-XII@proton.me>
2026-02-13 10:46:46 -0500

    class2 warmup

---
+++ CLASSWORK/Class_3/sketch.js
@@ -0,0 +1,33 @@
+function setup() {
+  createCanvas(500, 500);
+  background(100);
+  rect(110, 100, 1, 100)
+  rect(50, 150, 130, 350);
+  
+  let c = color(0, 70, 140);
+  fill(c);
+  rect(60, 200, 30, 30);
+  rect(100,200,30,30)
+  rect(60,250,30,30)
+  rect(100,250,30,30)
+  rect(140,250,30,30)
+  rect(100,300,30,30)
+  rect(140,300,30,30)
+  rect(60,350,30,30)
+  rect(100,350,30,30)
+  rect(140,350,30,30)
+
+  let c2 = color(242,234,24)
+  fill(c2);
+  rect(140,200,30,30)
+  rect(60,300,30,30)
+
+  let c3 = color(0,0,0)
+  fill(c3)
+  rect(100,460,30,40)
+
+
+  let c4 = color(255,0,0)
+  fill(c4)
+  square(105,90,10)
+}
