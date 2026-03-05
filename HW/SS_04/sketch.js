2b4f3a447daf8f0f0fcb86c9804ebef1f5ca82aa
O5-XII <05-XII@proton.me>
2026-03-05 10:42:24 -0500

    changed it to WEBGL

--- HW/SS_04/sketch.js
+++ HW/SS_04/sketch.js
@@ -1,6 +1,7 @@
 // variable setup/settings
 let wallImg;
 let expimg;
+let font;
 
 let wallvis = true;
 let expstart = -1;
@@ -14,16 +15,19 @@ const wallH = 220;
 function preload() {
   wallImg = loadImage("preload/brick-wall.png");
   expimg = loadImage("preload/explosion.png");
+  font = loadFont("preload/Inconsolata_Condensed-Regular.ttf");
 }
 
 //canvas create
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(400, 400, WEBGL);
   textAlign(CENTER, CENTER);
+  textFont(font);
 }
 //draw function
 function draw() {
   background(20);
+  translate(-width / 2, -height / 2);
 
   if (!wallvis) {
     drawWinText();
@@ -73,4 +77,6 @@ function drawWinText() {
   noStroke();
   textSize(54);
   text("YOU WIN", width / 2, height / 2);
+  textSize(20);
+  text("press f5", width / 2, height / 2 + 42);
 }
